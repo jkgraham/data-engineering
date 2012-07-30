73a69401c32363f128bf74d601179249bb3e873a
vagrant <vagrant@lucid32.(none)>
2012-07-30 10:04:01 -0700

    New rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lsocial::Application.config.secret_token = '177c2613903b43d257177b771d9154dc1319a20f7c5dd8249ff1686f98184870f6a97d21c00bf52c583275f422e61cc517275f0a110007aae8d64de6d434b35f'
